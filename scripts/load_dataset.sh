537ab2a334075ad3dde996c9ccf5226258706356
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2022-11-29 17:40:12 +0800

    what even (reinitialize new conda environment)

--- scripts/load_dataset.sh
+++ scripts/load_dataset.sh
@@ -1,5 +1,5 @@
 # Usage Instructions
-#    $ ./dataset.sh my_bucket_name directory_to_store_in
+#    $ bash load_dataset.sh my_bucket_name directory_to_store_in
 MY_BUCKET=$1
 mkdir $2
 gcsfuse --implicit-dirs --rename-dir-limit=100 --disable-http2 --max-conns-per-host=100 $MY_BUCKET $2
--- scripts/rapids_install.sh
+++ scripts/rapids_install.sh
@@ -1,2 +1,4 @@
+conda create --name gaitml python=3.8
+conda activate gaitml
 conda create -n rapids-22.10 -c rapidsai -c conda-forge -c nvidia  \
     rapids=22.10 python=3.8 cudatoolkit=11.5
