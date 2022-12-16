1c549efb44d205f0f5ee9b5f83e898b4acf2e4fe
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2022-12-16 09:02:47 +0800

    load dataset on google cloud

--- scripts/load_dataset.sh
+++ scripts/load_dataset.sh
@@ -1,6 +1,4 @@
 # Usage Instructions
 #    $ bash load_dataset.sh my_bucket_name directory_to_store_in
-MY_BUCKET=$1
-mkdir $2
-gcsfuse --implicit-dirs --rename-dir-limit=100 --disable-http2 --max-conns-per-host=100 $MY_BUCKET $2
-
+mkdir -p $2
+gcsfuse --implicit-dirs --rename-dir-limit=100 --disable-http2 --max-conns-per-host=100 $1 $2
